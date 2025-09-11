94cf578013595d050f34f81f1f789966f7cf8416
Kamiko Loss <info@gloxi.net>
2025-09-11 23:16:48 +0900

    refactor draw_ring to draw_circle

--- scenes/rail.gd
+++ scenes/rail.gd
@@ -18,20 +18,7 @@ func _ready() -> void:
 
 
 func _draw():
-    draw_ring(circle_radius, 64, Color.WHITE, -1.0)
-
-
-# ref. https://www.reddit.com/r/godot/comments/3ktq39/drawing_empty_circles_and_curves/
-func draw_ring(radius: float, resolution: int, color: Color, width: float, offset: Vector2 = Vector2.ZERO) -> void:
-    var increments := 2 * PI / resolution
-    var rad := 0.0
-    var to := Vector2.ZERO
-    var from := Vector2(cos(rad) * radius, sin(rad) * radius)
-    for i in resolution:
-        rad = increments * (i + 1)
-        to = Vector2(cos(rad) * radius, sin(rad) * radius)
-        draw_line(offset + from, offset + to, color, width)
-        from = to
+    draw_circle(Vector2.ZERO, circle_radius, Color.WHITE, false, -1.0, false)
 
 
 func start_rotate() -> void:
