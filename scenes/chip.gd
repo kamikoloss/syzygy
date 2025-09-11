563a55e9b089faa12ad6f46f74a2fc1b0516da8c
Kamiko Loss <info@gloxi.net>
2025-09-12 00:27:44 +0900

    update Chip to detect ChipSensor, ChipHolder

--- scenes/chip.gd
+++ scenes/chip.gd
@@ -1,2 +1,44 @@
 class_name Chip
 extends Control
+
+@export var area: Area2D
+
+
+func _ready() -> void:
+    area.area_entered.connect(_on_area_entered)
+    area.area_exited.connect(_on_area_exited)
+
+
+func _on_area_entered(other_area: Area2D) -> void:
+    var other_area_parent = other_area.get_parent()
+    if other_area_parent is ChipSensor:
+        _on_entered_chip_sensor(other_area_parent)
+    elif other_area_parent is ChipHolder:
+        _on_entered_chip_holder(other_area_parent)
+
+
+func _on_area_exited(other_area: Area2D) -> void:
+    var other_area_parent = other_area.get_parent()
+    if other_area_parent is ChipSensor:
+        _on_exited_chip_sensor(other_area_parent)
+    elif other_area_parent is ChipHolder:
+        _on_exited_chip_holder(other_area_parent)
+
+
+func _on_entered_chip_sensor(sensor: ChipSensor) -> void:
+    var tween := create_tween()
+    tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+    tween.tween_property(self, "modulate", Color.RED, 0.0)
+    tween.tween_property(self, "modulate", Color.WHITE, 1.0)
+
+
+func _on_entered_chip_holder(holder: ChipHolder) -> void:
+    pass
+
+
+func _on_exited_chip_sensor(sensor: ChipSensor) -> void:
+    pass
+
+
+func _on_exited_chip_holder(holder: ChipHolder) -> void:
+    pass
