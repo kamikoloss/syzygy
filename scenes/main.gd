c20c7c172fd1d7021cdc30096fd31650e38742ce
Kamiko Loss <info@gloxi.net>
2025-09-14 16:38:38 +0900

    add play and stop to Main (yet empty)

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,12 @@
 extends Control
 
+const PLAY_STOP_DURATION = 0.2
+
 @export var button_play: Button
 @export var button_stop: Button
 
-@export var grid_container: GridContainer
+@export var objects: Control
+@export var chip_storage: GridContainer
 @export var label_score_total: Label
 
 
@@ -16,6 +19,9 @@ var score := 0:
 func _ready() -> void:
     GlobalSignal.chip_entered_chip_sensor.connect(_on_chip_entered_chip_sensor)
     GlobalSignal.chip_fallen.connect(_on_chip_fallen)
+    button_play.pressed.connect(_on_button_play_pressed)
+    button_stop.pressed.connect(_on_button_stop_pressed)
+
     score = 0
 
 
@@ -28,3 +34,10 @@ func _on_chip_fallen(chip: Chip) -> void:
     # ストレージの Chip を元に戻す
     chip_storage.add_child(chip.duplicate())
 
+
+func _on_button_play_pressed() -> void:
+    print("[Main] play.")
+
+
+func _on_button_stop_pressed() -> void:
+    print("[Main] stop.")
