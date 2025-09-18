5d3be96677e95f93a41a59ddb180aeb88c46cd59
Kamiko Loss <info@gloxi.net>
2025-09-18 23:41:52 +0900

    add property of locked to ChipStorage

--- scenes/chip_storage.gd
+++ scenes/chip_storage.gd
@@ -1,6 +1,8 @@
 class_name ChipStorage
 extends Control
 
+const LOCKED_COLOR := Color(0.4, 0.4, 0.4)
+
 @export var _label_price: Label
 @export var _chips_parent: GridContainer
 
@@ -8,6 +10,13 @@ var price := 0:
     set(v):
         price = v
         _label_price.text = str(price)
+var locked := false:
+    set(v):
+        locked = v
+        if locked:
+            modulate = LOCKED_COLOR
+        else:
+            modulate = Color.WHITE
 
 
 func add_chip(chip: Chip) -> void:
