91c12a4d125fe3b6b6481ae2a6dd5426a859b677
Kamiko Loss <info@gloxi.net>
2025-09-19 23:41:59 +0900

    update flash of Chip

--- scenes/chip.gd
+++ scenes/chip.gd
@@ -7,18 +7,17 @@ signal dragged # (on: bool)
 const OUTLINE_COLOR_HOVER := Color.WHITE
 const OUTLINE_COLOR_CAN_RIDE := Color.GREEN
 const OUTLINE_COLOR_CAN_NOT_RIDE := Color.RED
-const FLASH_COLOR := Color.BLUE
-const FLASH_BACK_DURATION := 1.0
 
 @export var type := ChipData.Type.NONE:
     set(v):
         type = v
-        refresh_view()
+        _refresh_view()
 
-@export var area: Area2D
+@export var _area: Area2D
 
 @export var _texture_rect_main: TextureRect
 @export var _texture_rect_outline: TextureRect
+@export var _texture_rect_flash: TextureRect
 @export var _label_main: Label
 
 var price := 0
@@ -37,14 +36,16 @@ var _placed_holder: ChipHolder
 func _ready() -> void:
     mouse_entered.connect(_on_mouse_entered)
     mouse_exited.connect(_on_mouse_exited)
-    area.area_entered.connect(_on_area_entered)
-    area.area_exited.connect(_on_area_exited)
+    _area.area_entered.connect(_on_area_entered)
+    _area.area_exited.connect(_on_area_exited)
 
     center_offset = custom_minimum_size / 2.0
-    refresh_view()
+    _refresh_view()
+
+    _texture_rect_flash.self_modulate = Color.TRANSPARENT
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
     _label_main.rotation = get_global_transform().get_rotation() * -1
     if is_dragging:
         global_position = get_global_mouse_position() - center_offset
@@ -58,44 +59,12 @@ func _gui_input(event: InputEvent) -> void:
                 _drag(event.pressed)
 
 
-func refresh_view() -> void:
-    # Type
-    var data = ChipData.DATA[type] # [ <Label>, <Color>, <Price>, <Score> ]
-    _label_main.text = data[0] # "+%s" % [score]
-    _texture_rect_main.modulate = data[1]
-    price = data[2]
-    score = data[3]
-
-    # Locked
-    if is_locked:
-        pass
-    else:
-        pass
-
-    # Outline
-    _texture_rect_outline.visible = is_hovering or is_dragging
-    if is_dragging:
-        if is_overlapping and _overrapping_holder and not _overrapping_holder.is_placed:
-            _texture_rect_outline.modulate = OUTLINE_COLOR_CAN_RIDE
-        else:
-            _texture_rect_outline.modulate = OUTLINE_COLOR_CAN_NOT_RIDE
-    else:
-        _texture_rect_outline.modulate = OUTLINE_COLOR_HOVER
-
-
-func flash() -> void:
-    var tween := create_tween()
-    tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
-    tween.tween_property(self, "modulate", FLASH_COLOR, 0.0)
-    tween.tween_property(self, "modulate", Color.WHITE, FLASH_BACK_DURATION)
-
-
 func _on_mouse_entered() -> void:
     if type == ChipData.Type.ACCOUNT:
         return
     is_hovering = true
     hovered.emit(true)
-    refresh_view()
+    _refresh_view()
 
 
 func _on_mouse_exited() -> void:
@@ -103,7 +72,7 @@ func _on_mouse_exited() -> void:
         return
     is_hovering = false
     hovered.emit(false)
-    refresh_view()
+    _refresh_view()
 
 
 func _on_area_entered(other_area: Area2D) -> void:
@@ -124,14 +93,14 @@ func _on_area_exited(other_area: Area2D) -> void:
 
 func _on_entered_chip_sensor(chip_sensor: ChipSensor) -> void:
     GlobalSignal.chip_sensed.emit(self)
-    flash()
+    _flash()
 
 
 func _on_entered_chip_holder(chip_holder: ChipHolder) -> void:
     is_overlapping = true
     chip_holder.is_overlapped = true
     _overrapping_holder = chip_holder
-    refresh_view()
+    _refresh_view()
 
 
 func _on_exited_chip_sensor(sensor: ChipSensor) -> void:
@@ -141,7 +110,7 @@ func _on_exited_chip_sensor(sensor: ChipSensor) -> void:
 func _on_exited_chip_holder(holder: ChipHolder) -> void:
     is_overlapping = false
     holder.is_overlapped = false
-    refresh_view()
+    _refresh_view()
 
 
 func _drag(on: bool) -> void:
@@ -149,7 +118,7 @@ func _drag(on: bool) -> void:
     top_level = on
     rail_number = -1
     is_dragging = on
-    refresh_view()
+    _refresh_view()
 
     if _placed_holder:
         _placed_holder.is_placed = false
@@ -173,3 +142,30 @@ func _drag(on: bool) -> void:
             GlobalSignal.chip_fallen.emit(self)
             # TODO: 枠
             queue_free()
+
+
+func _refresh_view() -> void:
+    # Type
+    var data = ChipData.DATA[type] # [ <Label>, <Color>, <Price>, <Score> ]
+    _label_main.text = data[0] # "+%s" % [score]
+    _texture_rect_main.modulate = data[1]
+    price = data[2]
+    score = data[3]
+
+    # Outline
+    _texture_rect_outline.visible = is_hovering or is_dragging
+    if is_dragging:
+        if is_overlapping and _overrapping_holder and not _overrapping_holder.is_placed:
+            _texture_rect_outline.modulate = OUTLINE_COLOR_CAN_RIDE
+        else:
+            _texture_rect_outline.modulate = OUTLINE_COLOR_CAN_NOT_RIDE
+    else:
+        _texture_rect_outline.modulate = OUTLINE_COLOR_HOVER
+
+
+func _flash() -> void:
+    var tween := create_tween()
+    tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+    tween.tween_property(_texture_rect_flash, "self_modulate", Color.WHITE, 0.0)
+    tween.tween_interval(0.1)
+    tween.tween_property(_texture_rect_flash, "self_modulate", Color.TRANSPARENT, 1.0)
