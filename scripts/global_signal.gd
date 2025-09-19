e58ce64af5cc2f6739fbe0bf324881ba817da5ac
Kamiko Loss <info@gloxi.net>
2025-09-20 00:36:49 +0900

    chore

--- scripts/global_signal.gd
+++ scripts/global_signal.gd
@@ -1,6 +1,6 @@
 # GlobalSignal
 extends Node
 
-signal chip_sensed # (chip: Chip)
-signal chip_placed # (chip: Chip, chip_holder: ChipHolder)
-signal chip_fallen # (chip: Chip)
+signal chip_sensed(chip: Chip)
+signal chip_placed(chip: Chip, chip_holder: ChipHolder)
+signal chip_fallen(chip: Chip)
